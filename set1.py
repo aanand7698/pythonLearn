0c0fbbe531ea1355d557d8fce1b8d4001b775063
aanand7698 <149211798+aanand7698@users.noreply.github.com>
2024-07-10 16:54:36 +0000

    set section added

---
+++ set1.py
@@ -0,0 +1,13 @@
+#store following word meanings in a python dictionary
+
+mean1 = {
+    "table" : ["a piece of furniture" , "list of facts & figures"],
+    "cat" : "a small animal",
+}
+
+
+set0 = {"python","java","C++","python","javascript","java","python","java","C++","C"}
+
+#print(len(set0))
+
+#
