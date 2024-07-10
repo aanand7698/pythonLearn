36b7a405994c14fcbfdb0c2cfbf460434b298409
aanand7698 <149211798+aanand7698@users.noreply.github.com>
2024-07-10 08:56:49 +0000

    First Python program

---
+++ program1.py
@@ -0,0 +1,15 @@
+# write a program to ask the user to enter their 3 favorite movies and store them in a list.
+movies = []
+
+movies.append(input("enter 1st movie: "))
+movies.append(input("enter 2nd movie: "))
+movies.append(input("enter 3rd movie: "))
+
+print(movies)
+
+
+
+
+
+
+
---
+++ program3.py
@@ -0,0 +1,11 @@
+# WAP to count the number of students with the "A" grade in the following tuple.
+
+tup = ("C","D,","A","A","B","B","A")
+
+print(tup.count("A"))
+
+#WAP to sort the value in A and D.
+
+list1 =["C","D,","A","A","B","B","A"] 
+list1.sort()
+print(list1)
